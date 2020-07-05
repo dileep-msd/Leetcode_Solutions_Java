54c448c04263f75067bd46d21d5177ac1bdf3354
varnaa <swersie723>
2020-07-05 14:25:49 +0530

    Day 5.

---
+++ src/MonthlyChallenge/July/Day5.java
@@ -0,0 +1,12 @@
+package MonthlyChallenge.July;
+
+public class Day5 {
+    // Day 5
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 4;
+
+        int dis = a ^ b;
+        System.out.println(Integer.bitCount(dis));
+    }
+}
